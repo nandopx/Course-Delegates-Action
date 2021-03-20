9085b50b9e187534d9f32b46c5d0991cf9e38073
Jesse Fernando <nandopox@gmail.com>
2021-03-19 23:00:55 -0300

    Working with predicate reference

--- Course-Delegates-Action/Program.cs
+++ Course-Delegates-Action/Program.cs
@@ -15,7 +15,7 @@ namespace Course_Delegates_Action
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = UpdatePrice;
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
             list.ForEach(act);
 
