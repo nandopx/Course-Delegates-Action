97a5a5b5d413b98e0ddaf8e9b35b2024da3d91f5
Jesse Fernando <nandopox@gmail.com>
2021-03-19 22:56:33 -0300

    Working with reference method

--- Course-Delegates-Action/Program.cs
+++ Course-Delegates-Action/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Course_Delegates_Action.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace Course_Delegates_Action
 {
@@ -6,7 +8,24 @@ namespace Course_Delegates_Action
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
