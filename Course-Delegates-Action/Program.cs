808a8ebf802ae7203c2670da00a91326c6647000
Jesse Fernando <nandopox@gmail.com>
2021-03-19 22:49:42 -0300

    Project created

---
+++ Course-Delegates-Action/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course_Delegates_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
