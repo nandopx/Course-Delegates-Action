80e6b781dc98f1a51a7472cc42e3e8eb9bcb4257
Jesse Fernando <nandopox@gmail.com>
2021-03-19 23:02:30 -0300

    Working with lambda predicate 'in line'

--- Course-Delegates-Action/Program.cs
+++ Course-Delegates-Action/Program.cs
@@ -15,19 +15,11 @@ namespace Course_Delegates_Action
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = p => { p.Price += p.Price * 0.1; };
-
-            list.ForEach(act);
-
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
-        }
-
-        static void UpdatePrice(Product p)
-        {
-            p.Price += p.Price * 0.1;
         }
     }
 }
